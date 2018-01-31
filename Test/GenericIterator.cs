727982bc2bc6410d595090b9819726cf58329e56
m7nu3l <mgcarrasco2012@gmail.com>
2018-01-31 13:24:18 -0300

    Added examples.

---
+++ Test/GenericIterator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    static class GenericIterator
+    {
+        /*static void Main()
+        {
+            var values = new[] { "a", "b", "c" };
+            values.ForEach((pos, s) => Console.WriteLine("{0}: {1}", pos, s));
+        }
+
+        static void ForEach<T>(
+                this IEnumerable<T> source,
+                Action<int, T> action)
+        {
+            if (source == null) throw new ArgumentNullException("source");
+            if (action == null) throw new ArgumentNullException("action");
+
+            int position = 0;
+            foreach (T item in source)
+            {
+                action(position++, item);
+            }
+        }*/
+    }
+}
---
+++ Test/List.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class List
+    {
+        /*public void foo()
+        {
+            var l = new LinkedList<int>();
+
+            l.AddFirst(1);
+            l.AddFirst(2);
+            l.AddFirst(3);
+            l.AddFirst(4);
+
+            int acum = 0;
+            foreach (var item in l)
+            {
+                acum = acum + item;
+            }
+        }*/
+
+    }
+}
