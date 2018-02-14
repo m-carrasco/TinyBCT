4564c950a389d20af17ac564b4c90936c3f505b0
Diego <diego.garbervetsky@gmail.com>
2018-02-13 19:14:50 -0800

    Comment this other classes that produces bpl that fail in Corral

--- Test/List.cs
+++ Test/List.cs
@@ -4,32 +4,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Test
-{
-    class List
-    {
-        public void foo()
-        {
-            var l = new LinkedList<int>();
+//namespace Test
+//{
+//    class List
+//    {
+//        public void foo()
+//        {
+//            var l = new LinkedList<int>();
 
-            l.AddFirst(1);
-            l.AddFirst(2);
-            l.AddFirst(3);
-            l.AddFirst(4);
+//            l.AddFirst(1);
+//            l.AddFirst(2);
+//            l.AddFirst(3);
+//            l.AddFirst(4);
 
-            int acum = 0;
-            foreach (var item in Elems(l))
-            {
-                acum = acum + item;
-            }
-        }
-		static IEnumerable<int> Elems(ICollection<int> list)
-		{
-			foreach (var elem in list)
-			{
-				yield return elem;
+//            int acum = 0;
+//            foreach (var item in Elems(l))
+//            {
+//                acum = acum + item;
+//            }
+//        }
+//		static IEnumerable<int> Elems(ICollection<int> list)
+//		{
+//			foreach (var elem in list)
+//			{
+//				yield return elem;
 					 
-			}
-		}
-	}
-}
+//			}
+//		}
+//	}
+//}
