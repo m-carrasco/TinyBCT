34755d86075a8bc84b9f1ed15b7821491f461c75
Rodrigo Castano <castano.rodrigo@gmail.com>
2018-05-02 23:02:55 -0300

    Adding code to be compiled within test

--- TinyBCT/Tests/Tests.cs
+++ TinyBCT/Tests/Tests.cs
@@ -63,4 +63,64 @@ class TestAs
             Assert.Fail();
         }
     }
+
+    [TestMethod]
+    public void GenericsTest()
+    {
+        var source = @"
+using System;
+using System.Diagnostics.Contracts;
+class Holds {
+ public int getValue() {
+  return 5;
+ }
+}
+class Holds<T> {
+ T value;
+ public Holds() {
+  value = default(T);
+ }
+ public Holds(T new_val) {
+  value = new_val;
+ }
+ public void setValue(T new_val) {
+  value = new_val;
+ }
+ // Will not compile:
+ // Program.cs(20,11): error CS0019: Operator '+' cannot be applied to operands of type 'T' and 'T'
+ //public void applyOpPlus(T to_add) {
+ // value = value + to_add;
+ //}
+ public T getValue() {
+  return value;
+ }
+}
+
+// Relevant reference:
+// https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/generics/differences-between-cpp-templates-and-csharp-generics
+
+class GenericsMain {
+  public static void Main() {
+    Holds<int> holds_default_int = new Holds<int>();
+    Contract.Assert(holds_default_int.getValue() != 9);
+    Holds<int> holds_int_1 = new Holds<int>(1);
+    Contract.Assert(holds_int_1.getValue() == 1);
+    Holds<int> holds_int_2 = new Holds<int>(2);
+	// BCT fails assertion, as it should
+    // Contract.Assert(holds_int_2.getValue() == 1);
+	
+	Holds<string> holds_default_string = new Holds<string>();
+    // Contract.Assert(!holds_default_string.getValue().Equals(""Hello""));
+  }
+}
+            ";
+        if (Test.TestUtils.CreateAssemblyDefinition(source, "GenericsTest"))
+        {
+            TinyBCT.Program.Main(new string[] { "-i", "GenericsTest.dll" });
+        }
+        else
+        {
+            Assert.Fail();
+        }
+    }
 }
