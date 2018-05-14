b02baeabd1b85ab1e37c651f97e920ef460e2531
m7nu3l <mgcarrasco2012@gmail.com>
2018-05-14 14:50:39 -0300

    Added example for arrays.

---
+++ Test/Arrays.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class Arrays
+    {
+        static void Main(string[] args)
+        {
+            Foo f = new Foo();
+            f.i = 5;
+
+            int x = f.i;
+            x = x + 1;
+
+            f.p = args[0];
+        }
+
+        /*static void arrayTest1(Foo[] a)
+        {
+            int l0 = a.GetLength(0);
+            int l1 = a.GetLength(1);
+            int l2 = a.Count();
+            int l3 = a.Length;
+
+            Object v1 = a.GetValue(0); // this causes an exception in tinybct
+            Foo v2 = a[0];
+        }*/
+
+        static void arrayTest2(Foo[][] a)
+        {
+            Foo v2 = a[0][2];
+        }
+
+        static void arrayTest3()
+        {
+            var a = new Foo[510, 10];
+            Foo v2 = a[0, 2];
+        }
+
+        static void arrayTest4()
+        {
+            var a = new Foo[510];
+            Foo v2 = a[0];
+        }
+
+        static void arrayTest5()
+        {
+            var a = new Foo[510];
+            a[0] = new Foo();
+        }
+
+        static void arrayTest6()
+        {
+            var a = new int[510];
+            a[0] = 30;
+        }
+    }
+
+    class Foo
+    {
+        public int i;
+        public string p;
+    }
+}
