e9bf0b1a7977beffecaef3f917c1429f6c00e560
m7nu3l <mgcarrasco2012@gmail.com>
2018-06-28 16:14:52 -0300

    Removed lines to make the test conciser

--- Test/Boxing.cs
+++ Test/Boxing.cs
@@ -45,22 +45,11 @@ namespace Test
             // it makes the assert false unreachable
 
             List<Object> list = new List<Object>();
-            list.Add(1);
-            list.Add(false);
-            list.Add(1.5);
-            list.Add(1.5f);
-
-            int pos0 = (int)list[0];
-            bool pos1 = (bool)list[1];
-            double pos2 = (double)list[2];
-            float pos3 = (float)list[3];
-
-            Contract.Assert(pos0 == 1);
-            Contract.Assert(pos1 == false);
-            Contract.Assert(pos2 == 1.5);
-            Contract.Assert(pos3 == 1.5f);
-
+            list.Add(false); // here false in tac is a zero
             Contract.Assert(false);
+            bool pos1 = (bool)list[1];
+
+            //Contract.Assert(false);
         }
     }
 }
