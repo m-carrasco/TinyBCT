05fcf2b9a0ece722b76269c31ebd76bd5972a5ea
m7nu3l <mgcarrasco2012@gmail.com>
2018-07-17 17:02:15 -0300

    Temp work for ref keyword. Not finished.

--- Test/RefKeyword.cs
+++ Test/RefKeyword.cs
@@ -9,6 +9,8 @@ namespace Test
 {
     class RefKeyword
     {
+        public int field;
+
         public static void Ref(ref int a, ref int b)
         {
             a = 10;
@@ -41,6 +43,13 @@ namespace Test
             Ref(ref i, ref j);
 
             Contract.Assert(i == 10 && j == 11);
+        }
+
+        static void TestField(RefKeyword r)
+        {
+            // r1 = r.field;
+            Ref(ref r.field, ref r.field);
+            Contract.Assert(r.field == 11);
         }
     }
 }
