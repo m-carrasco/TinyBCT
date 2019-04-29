68f66daadbf56b35373f48c061614c12511e8165
Michael Emmi <michael.emmi@gmail.com>
2019-04-29 10:58:58 -0400

    Fixed relative paths.

--- mono/build-deps.sh
+++ mono/build-deps.sh
@@ -5,7 +5,9 @@ set -e
 (cd cci && msbuild Samples/PeToPe/PeToPe.csproj /t:Build)
 
 # Overwwrite the dependencies with newly-built DLLs
-for d in TinyBCT/Dependencies/Microsoft.Cci.*
+for d in TinyBCT/Dependencies/Microsoft.Cci.*.dll
 do
-	ln -fs $(find ~/Code/cci/Samples/PeToPe -name $(basename $d)) TinyBCT/Dependencies/$(basename $d)
+	name=$(basename $d)
+	dir=$(cd $(dirname $(find cci/Samples/PeToPe -name $name)) && pwd)
+	ln -fs $dir/$name TinyBCT/Dependencies/$name
 done
