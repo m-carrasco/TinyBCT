1eeebfaffbcca756387470d171bf7c6b40c854d1
m7nu3l <mcarrasco@dc.uba.ar>
2019-07-10 17:03:20 -0400

    Added option to timeout.

---
+++ TinyBCT/Utils/Timeout.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+
+namespace TinyBCT.Utils
+{
+    public class Timeout
+    {
+        public Timeout()
+        {
+        }
+
+        public static void InitiateTimeoutCheck()
+        {
+            Thread t = new Thread(() => CheckTimeout(Settings.TimeoutMinutes));
+            t.Start();
+        }
+        private static void CheckTimeout(int timeInMinutes)
+        {
+            Thread.Sleep(timeInMinutes * 60 * 1000);
+            throw new TimeoutException();
+        }
+    }
+}
